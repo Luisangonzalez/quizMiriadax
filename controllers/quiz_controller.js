c4e4b51f4fb11f1611071660baca8a246afaaebd
luisangonzalez@gmail.com <luisangonzalez@gmail.com>
2015-07-09 00:42:22 +0200

    add creditos && author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,3 +10,7 @@ exports.answer = function(req, res) {
     res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
 };
+//Get /author
+exports.author = function(req, res){
+  res.render('author');
+};
