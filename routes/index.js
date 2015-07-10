06c99bf2990c26dbfa00f3405d13bea82e658de6
luisangonzalez@gmail.com <luisangonzalez@gmail.com>
2015-07-10 20:38:07 +0200

    Modulo 7 add autload and list questions

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ var quizController = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Bienvenido a Quiz' });
+  res.render('index', { title: 'Bienvenido a Quiz',errors: [] });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload :quizId
+router.param('quizId', quizController.load);
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author',quizController.author);
 
 module.exports = router;
