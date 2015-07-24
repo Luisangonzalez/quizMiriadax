b76215cea6fdb2342c736b13c7e2fc6f4a0d5694
luisangonzalez@gmail.com <luisangonzalez@gmail.com>
2015-07-24 21:07:47 +0200

    fix some error

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -148,6 +148,12 @@ exports.update = function(req, res) {
 }
 };
 
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy()
+  .then(function() {res.redirect('/quizes');})
+  .catch( function(error) {next(error);});
+};
 
 //Get /author
 exports.author = function(req, res){
